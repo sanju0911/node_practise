ce8969c79ca7f5b064fb9eee0a6faef07e50d208
sanju0911 <97456105+sanju0911@users.noreply.github.com>
2023-08-23 13:12:05 +0530

    Add files via upload
    ADDED WITH DISPLAYING MESSAGE

---
+++ app.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const fs = require('fs');
+const { buffer } = require('stream/consumers');
+
+const server = http.createServer((req , res) =>{
+    const method = req.method;
+    if (req.url === '/') {
+        const messages = fs.readFileSync('message.txt', 'utf-8');
+        res.write('<html><body>');
+      
+       
+        messages.split('\n').forEach(message => {
+          if (message.trim() !== '') {
+            res.write(message);
+          }
+        });
+      
+        res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form>');
+        res.write('</body></html>');
+        return res.end();
+      }
+
+   if(req.url ==="/message" && method === "POST")
+   {
+    const body = [];
+    req.on('data',(chunk)=>{
+        console.log(chunk);
+          body.push(chunk);
+    });
+    req.on('end', ()=>{
+        const parsedbody = Buffer.concat(body).toString();
+        const message = parsedbody.split("=")[1];
+        fs.writeFileSync('message.txt' , message);
+    
+    });
+   
+    res.statusCode = 302;
+    res.setHeader('Location','/');
+    return res.end();
+   }
+  
+   res.end();
+});
+
+server.listen(5000);
