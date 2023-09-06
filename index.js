2859e1bb2640962d58d5e94adc93122c006c1758
sanju0911 <97456105+sanju0911@users.noreply.github.com>
2023-09-06 19:01:27 +0530

    added controllers

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const body = require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes =  require('./routes/shop');
 const ContactUs = require('./routes/contactus');
+
+
 const path = require('path');
 const express = require('express')
+
+
 
 const app = express();
 app.use(body.urlencoded({extended : true}));
@@ -19,6 +23,7 @@ app.use('/Sucess',(req,res)=>{
 
     res.send("submitted successfully");
 })
+
 app.use((req,res)=>{
 
     res.status(404).sendFile(path.join(__dirname,'views','not.html'));
